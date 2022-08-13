e48e4d185fe440b4fefefa5bd3bb07f5ea7a3369
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-13 22:42:02 +0530

    [Saravanan]: Added program to bet

--- src/Day7/BridgeLabz/Gambler.java
+++ src/Day7/BridgeLabz/Gambler.java
@@ -1,13 +1,27 @@
 package Day7.BridgeLabz;
 /*
- * Testing Git
+ * This program Gambler, will bet $1
  */
 
 public class Gambler {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambler Program");
-
+		/*
+		 * Variables
+		 */
+		int stake = 100;
+		int bet = 1;
+		/*
+		 * Random variable to see win or loose
+		 */
+		double win = Math.floor(Math.random() * 10) % 2;
+		/*
+		 * betting $1 from stake
+		 */
+		System.out.println("Betting $1 from stake "+ stake);
+		System.out.println("remaining stake: "+(stake -= bet));
+		
 	}
 
 }
