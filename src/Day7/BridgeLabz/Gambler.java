a59cb7b2a357b10eee8ce2c61b9d520bc3ca5c0b
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-13 21:28:04 +0530

    [Saravanan]: Added program to print Welcome message

---
+++ src/Day7/BridgeLabz/Gambler.java
@@ -0,0 +1,13 @@
+package Day7.BridgeLabz;
+/*
+ * Testing Git
+ */
+
+public class Gambler {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambler Program");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Saravanan
+ *
+ */
+module GamblingSimulator {
+}
