1b0f9ece640e274bbad47b04ddd487ef3bf42359
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-14 22:19:38 +0530

    [Saravanan]: Added program to see gambler win or loose for a day

--- src/Day7/BridgeLabz/Gambler.java
+++ src/Day7/BridgeLabz/Gambler.java
@@ -1,6 +1,7 @@
 package Day7.BridgeLabz;
 /*
- * This program checking if gambler win or loose
+ * This program If gambler won or lost 50% of the stake,
+ * would resign for the day
  */
 
 public class Gambler {
@@ -12,23 +13,33 @@ public class Gambler {
 		 */
 		int stake = 100;
 		int bet = 1;
+		int round = 0;
 		
+		int trials = stake;
 		/*
-		 * betting $1 from stake
+		 * Taking while loop with condition won or lost 50% of the stake
 		 */
-		System.out.println("Betting $1 from stake "+ stake);
-		System.out.println("remaining stake: "+(stake -= bet));	
-		/*
-		 * Random variable for win or loose
-		 */
-		double win = Math.floor(Math.random() * 10) % 2;
-		 
-		if (win == 1) {
-			stake += (bet *2);
-			System.out.println("Congratulations! You won $1");
-		}else {
-			System.out.println("Sorry! You loose $1");
-		}System.out.println("The stake we have: "+stake);
+		while ((stake > (trials/2)) && stake < (trials+(trials/2))) {
+			round++;
+			System.out.println("round "+round);
+			/*
+			 * betting $1 from stake
+			 */
+			System.out.println("Betting $1 from stake "+ stake);
+			System.out.println("remaining stake: "+(stake -= bet));	
+			/*
+			 * Random variable for win or loose
+			 */
+			int win = (int) Math.floor(Math.random() * 10) % 2;
+			if (win == 1) {
+				stake += (bet *2);
+				System.out.println("Congratulations! You won $1");
+			}else {
+				System.out.println("Sorry! You loose $1");
+			}System.out.println("The stake we have: "+stake);
+			
+		}
+		System.out.println("Resigning for the day"+stake);		
 	}
 
 }
