85f82c671e3da1f6d6086d4ab1aef66b95ca1feb
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-16 23:59:00 +0530

    [Saravanan]: Added program to find how much won or lost monthly

--- src/Day7/BridgeLabz/Gambler.java
+++ src/Day7/BridgeLabz/Gambler.java
@@ -1,12 +1,10 @@
 package Day7.BridgeLabz;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
 /*
  * This program If gambler won or lost 50% of the stake,
  * would resign for the day
  * For 20 days tracking every day win or loose 
+ * For 12 months tracking how much days won and lost 
  */
 
 public class Gambler {
@@ -19,37 +17,57 @@ public class Gambler {
 		/*
 		 * Variables
 		 */
-		int stake = 100;
-		int trials = 100;			
-		for (int day = 1; day<=NUM_OF_DAYS; day++) {
-			trials = stake;
-			/*
-			 * Taking while loop with condition won or lost 50% of the stake
-			 */
-			while ((stake > (trials/2)) && stake < (trials+(trials/2))) {
+		double stake = 100;
+		double trials = 100;
+		int month = 0;
+		while (month < 12) {
+			month++;
+			int won = 0;
+			int lost = 0;
+			
+			for (int day = 1; day<=NUM_OF_DAYS; day++) {
 				/*
-				 * betting $1 from stake
+				 * Taking stake as 100 if stake becomes zero
 				 */
-				stake -= BET;	
-				/*
-				 * Random variable for win or loose
-				 */
-				int win = (int) Math.floor(Math.random() * 10) % 2;
-				switch (win) {
-				case 1:
-					stake += (BET *2);
-					break;
-				default:
-					break;
+				if (stake == 0) {
+					stake = stake + 100;
+					System.out.println("Stake got 100$ "+stake);
 				}
+				trials = stake;
+				/*
+				 * Taking while loop with condition won or lost 50% of the stake
+				 */
+				while ((stake > (trials/2)) && stake < (trials+(trials/2))) {
+					/*
+					 * betting $1 from stake
+					 */
+					stake -= BET;	
+					/*
+					 * Random variable for win or loose
+					 */
+					int win = (int) Math.floor(Math.random() * 10) % 2;
+					switch (win) {
+					case 1:
+						stake += (BET *2);
+						break;
+					default:
+						break;
+					}
+				}
+				if (stake > trials) {
+					System.out.println("Day# Gambling Day: " + day + " Won: " + Math.abs(trials - stake));
+					won+=1;
+				}else { 
+					System.out.println("Day# Gambling Day: " + day + " Loose: " + Math.abs(trials - stake));
+					lost+=1;
+				}
+				
 			}
-			if (stake > trials) {
-				System.out.println("Day# Gambling Day: " + day + " Won: " + Math.abs(trials - stake));
-			}else { 
-				System.out.println("Day# Gambling Day: " + day + " Loose: " + Math.abs(trials - stake));
-			}
-		
+			System.out.println("Month# Gambling Month: " + month + " Total "+ won + " Won" );
+			System.out.println("Month# Gambling Month: " + month + " Total "+ lost + " Loose");
+			System.out.println();
 		}
+	
 	}
 
 
